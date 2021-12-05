da8ef8d9d2a6d216361322d09b212310a68a9498
leandrokira <73450242+leandrokira@users.noreply.github.com>
2021-12-04 23:50:27 -0300

    consegui fazer o login, falta o cadastro
    consegui fazer o login, falta o cadastro

--- index.php
+++ index.php
@@ -1,148 +1,58 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php
+session_start();
+?>
 
+<!DOCTYPE html>
+<html>
+    
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-    <title>Untitled</title>
-    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/styles.css">
-    <link rel="stylesheet" type="text/css" href="style.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Sistema de Login - PHP + MySQL - Canal TI</title>
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/bulma.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/login.css">
 </head>
 
-<body style="padding: 20px;">
-<?php
+<body>
+    <section class="hero is-success is-fullheight">
+        <div class="hero-body">
+            <div class="container has-text-centered">
+                <div class="column is-4 is-offset-4">
+                    <h3 class="title has-text-grey">Sistema de Login</h3>
+                    <h3 class="title has-text-grey"><a href="https://youtube.com/canaltioficial" target="_blank">Canal TI</a></h3>
+                    <?php
+                      if(isset($_SESSION['nao_autentificado']))
+                          echo "<div class='notification is-danger'>
+                          <p>ERRO: Usuário ou senha inválidos.</p>
+                        </div>"
+                      
+                    ?>
 
- include("audioConexao.php");
- $consultaAudio = "SELECT * FROM chataudio";
- $conAudio = $conn->query($consultaAudio) or die($conn->error);
+                    <?php
+                    unset($_SESSION['nao_autentificado']);
+                    ?>
+                    <div class="box">
+                        <form action="login.php" method="POST">
+                            <div class="field">
+                                <div class="control">
+                                    <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
+                                </div>
+                            </div>
 
- $consultaAudioStory = "SELECT * FROM chataudiostory";
- $conAudioStory = $conn->query($consultaAudioStory) or die($conn->error);
-
-?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script
-        src='https://code.jquery.com/jquery-3.2.1.slim.js'
-        integrity='sha256-tA8y0XqiwnpwmOIl3SGAcFl2RvxHjA8qp0+1uCGmRmg='
-        crossorigin='anonymous'
-    ></script>
-    <div class="row" >
-
-
-        <button type="button" id="btnStory">Gravar Story</button>
-        <form name="form1" method="POST" action="audioEnvioStory.php">
-            <input name="audio" type="text" id="teste" style="display:none"></p>
-            <input type="submit" value="confirmar"/>
-        </form>
-        <?php 
-            while($dado = $conAudioStory->fetch_array() ){
-            ?>      
-            <div id="postAudioStory"class="row">
-                <audio id="audiostory" class="<?php echo $dado["id"] ?>" src="<?php echo $dado["audio"] ?>" controls=""></audio>
-            </div>
-        <?php }?>
-    </div>
-    <hr/>
-    <div class="row">
-        <div class="col-md-10">
-            <button type="button" id="btnStart">Gravar</button>
-                <form name="form1" method="POST" action="audioEnvio.php">
-                    <input name="audio" type="text" id="teste" style="display:none"></p>
-
-                    <input type="submit" value="confirmar"/>
-                </form>
-            <p>LISTA</p>
-      
-                <?php 
-                    while($dado = $conAudio->fetch_array() ){
-                ?>
-                    <div id="postAudio" class="row border">
-                        <script>
-                        //Quando se clica no player, toca o áudio.
-                            function play<?php echo $dado["id"] ?>(){
-                                var audio = document.querySelector(".a<?php echo $dado["id"] ?>");;
-                                audio.play();
-                            }
-                        </script>
-                        <div class="row" style="padding-left:20px;">
-                            <p id="conteudoPostAudio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur facilis deleniti atque maiores </p>
-                            
-                        </div>
-                        
-                        <div class="row">
-                        <!-- IMAGEM DE PLAYER-->
-                            <img src="img/play.png" style="width: 100px;"onclick="play<?php echo $dado['id'] ?>()">
-                            <audio class="a<?php echo $dado["id"] ?>" src="<?php echo $dado["audio"] ?>" controls="" style="display:none"></audio>
-                        </div>
-
+                            <div class="field">
+                                <div class="control">
+                                    <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
+                                </div>
+                            </div>
+                            <button type="submit" class="button is-block is-link is-large is-fullwidth">Entrar</button>
+                        </form>
                     </div>
-            <?php }?>
+                </div>
+            </div>
         </div>
-        <div class="row"></div>
-        <div class="col">
-        <div id="nameLogin" class="row">lo</div>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos qui sed modi deleniti beatae at repellendus cum, repellat architecto, tempora iste nostrum, corrupti suscipit! Dolor at earum sunt accusantium nihil!</div>  
-    </div>
-
-    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-    <script>
-        $(function(){
-            let mediaRecorder
-
-            navigator.mediaDevices.getUserMedia({ audio: true }).then(
-          (stream) => {
-            mediaRecorder = new MediaRecorder(stream)
-            let chunks = []
-            mediaRecorder.ondataavailable = data => {
-                chunks.push(data.data)
-            }
-            mediaRecorder.onstop = () => {
-                const blob = new Blob(chunks, { type: 'audio/ogg; code=opus' })
-  const reader = new window.FileReader()
-  reader.readAsDataURL(blob)
-  reader.onloadend = () => {
-    const audio = document.createElement('audio')
-    audio.src = reader.result
-    audio.controls = true
-    $('body').append(audio)
-    $teste = reader.result;
-            function open_popup(date_today) {
-            document.querySelector("#teste").value = date_today;
-            }
-            open_popup($teste);
-  }
-
-            }
-            $('#btnStart').click(function(){
-         
-         if($(this).text() === 'Gravar'){       
-             mediaRecorder.start()
-             $(this).text('Gravando')
-         }else{
-             mediaRecorder.stop() 
-             $(this).text('Gravar') 
-         }
-     })
-     $('#btnStory').click(function(){
-         
-         if($(this).text() === 'Gravar Story'){       
-             mediaRecorder.start()
-             $(this).text('Gravando')
-         }else{
-             mediaRecorder.stop() 
-             $(this).text('Gravar Story') 
-         }
-     })
-          },
-          (err) => {
-            $('body').append('<p>Você deve permitir o áudio</p>')
-          })   
-              
-        })
-      </script>
-
-
+    </section>
 </body>
 
 </html>
---
+++ painel.php
@@ -0,0 +1,158 @@
+<?php
+session_start();
+include('verifica_login.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Untitled</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/css/styles.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+</head>
+
+<body style="padding: 20px;">
+<?php
+
+ include("audioConexao.php");
+ $consultaAudio = "SELECT * FROM chataudio";
+ $conAudio = $conn->query($consultaAudio) or die($conn->error);
+
+ $consultaAudioStory = "SELECT * FROM chataudiostory";
+ $conAudioStory = $conn->query($consultaAudioStory) or die($conn->error);
+
+?>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script
+        src='https://code.jquery.com/jquery-3.2.1.slim.js'
+        integrity='sha256-tA8y0XqiwnpwmOIl3SGAcFl2RvxHjA8qp0+1uCGmRmg='
+        crossorigin='anonymous'
+    ></script>
+    <div class="row" >
+
+
+        <button type="button" id="btnStory">Gravar Story</button>
+        <form name="form1" method="POST" action="audioEnvioStory.php">
+            <input name="audio" type="text" id="teste" style="display:none"></p>
+            <input type="submit" value="confirmar"/>
+        </form>
+        <?php 
+            while($dado = $conAudioStory->fetch_array() ){
+            ?>      
+            <div id="postAudioStory"class="row">
+                <audio id="audiostory" class="<?php echo $dado["id"] ?>" src="<?php echo $dado["audio"] ?>" controls=""></audio>
+            </div>
+        <?php }?>
+    </div>
+    <hr/>
+    <div class="row">
+        <div class="col-md-10">
+            <button type="button" id="btnStart">Gravar</button>
+                <form name="form1" method="POST" action="audioEnvio.php">
+                    <input name="audio" type="text" id="teste" style="display:none"></p>
+
+                    <input type="submit" value="confirmar"/>
+                </form>
+            <p>LISTA</p>
+      
+                <?php 
+                    while($dado = $conAudio->fetch_array() ){
+                ?>
+                    <div id="postAudio" class="row border">
+                        <script>
+                        //Quando se clica no player, toca o áudio.
+                            function play<?php echo $dado["id"] ?>(){
+                                var audio = document.querySelector(".a<?php echo $dado["id"] ?>");;
+                                audio.play();
+                            }
+                        </script>
+                        <div class="row" style="padding-left:20px;">
+                            <p id="conteudoPostAudio">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur facilis deleniti atque maiores </p>
+                            
+                        </div>
+                        
+                        <div class="row">
+                        <!-- IMAGEM DE PLAYER-->
+                            <img src="img/play.png" style="width: 100px;"onclick="play<?php echo $dado['id'] ?>()">
+                            <audio class="a<?php echo $dado["id"] ?>" src="<?php echo $dado["audio"] ?>" controls="" style="display:none"></audio>
+                        </div>
+
+                    </div>
+            <?php }?>
+        </div>
+        <div class="row"></div>
+        <div class="col border">
+             <div id="nameLogin" class="border">
+                <h2>Olá, <?php echo $_SESSION['usuario'];?></h2>
+                 <h2><a href="logout.php">Sair</a></h2>
+            </div>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos qui sed modi deleniti beatae at repellendus cum, repellat architecto, tempora iste nostrum, corrupti suscipit! Dolor at earum sunt accusantium nihil!
+        </div>  
+        </div>
+  
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script>
+        $(function(){
+            let mediaRecorder
+
+            navigator.mediaDevices.getUserMedia({ audio: true }).then(
+          (stream) => {
+            mediaRecorder = new MediaRecorder(stream)
+            let chunks = []
+            mediaRecorder.ondataavailable = data => {
+                chunks.push(data.data)
+            }
+            mediaRecorder.onstop = () => {
+                const blob = new Blob(chunks, { type: 'audio/ogg; code=opus' })
+  const reader = new window.FileReader()
+  reader.readAsDataURL(blob)
+  reader.onloadend = () => {
+    const audio = document.createElement('audio')
+    audio.src = reader.result
+    audio.controls = true
+    $('body').append(audio)
+    $teste = reader.result;
+            function open_popup(date_today) {
+            document.querySelector("#teste").value = date_today;
+            }
+            open_popup($teste);
+  }
+
+            }
+            $('#btnStart').click(function(){
+         
+         if($(this).text() === 'Gravar'){       
+             mediaRecorder.start()
+             $(this).text('Gravando')
+         }else{
+             mediaRecorder.stop() 
+             $(this).text('Gravar') 
+         }
+     })
+     $('#btnStory').click(function(){
+         
+         if($(this).text() === 'Gravar Story'){       
+             mediaRecorder.start()
+             $(this).text('Gravando')
+         }else{
+             mediaRecorder.stop() 
+             $(this).text('Gravar Story') 
+         }
+     })
+          },
+          (err) => {
+            $('body').append('<p>Você deve permitir o áudio</p>')
+          })   
+              
+        })
+      </script>
+
+
+</body>
+
+</html>
