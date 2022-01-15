ee175d986111a8f4ff418d7ce9b35649e91c9a2b
leandrokira <73450242+leandrokira@users.noreply.github.com>
2022-01-14 21:52:15 -0300

    MENU
    Adicionei alguns itens no menu

--- painel.php
+++ painel.php
@@ -89,7 +89,11 @@ include('verifica_login.php');
         <div class="col border">
              <div id="nameLogin" class="border">
                 <h2>Olá, <?php echo $_SESSION['usuario'];?></h2>
+                <!-- MENU-->
                  <h2><a href="logout.php">Sair</a></h2>
+                 <h2><a href="404.php">Lista de Amigos</a></h2>
+                 <h2><a href="404.php">Adiconar Pessoas</a></h2>
+                <!--FIM MENU-->
             </div>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos qui sed modi deleniti beatae at repellendus cum, repellat architecto, tempora iste nostrum, corrupti suscipit! Dolor at earum sunt accusantium nihil!
         </div>  
