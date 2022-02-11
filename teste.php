1ae072c37c188ecaf1571fe47568f53efce9e2b6
leandrokira <73450242+leandrokira@users.noreply.github.com>
2022-02-11 00:44:31 -0300

    Painel de audio melhorado

--- teste.php
+++ teste.php
@@ -10,10 +10,21 @@
   <title>Document</title>
 </head>
 <body>
-    <div class="border" style="width:450px; height:300px; margin: 100px">
-        <img class="border rounded-circle" src="img/play.png" style="max-width: 50px;max-height:50px; margin-left:10px; margin-top:10px;margin-right:10px">
-        <span id="nomePainelAudio">kkk</span>
+    <div class="border " style="width:450px; height:300px; margin: 100px">
+        <!-- BLOCO 1 : IMAGEM E NOME -->
+        <div id="bloco1PainelAudio">
+        <img id="imagemPainelAudio" class="border rounded-circle border-dark" src="img/play.png" style="">
+        <span id="nomePainelAudio">Lawliet36</span>
         <hr>
+        </div>
+        <!-- ####################### -->
+        <!-- BLOCO 1 : POST TEXTO -->
+        <div id="bloco2PainelAudio">
+            <span id="postPainelAudio">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Iure sit suscipit esse omnis repellendus. Debitis ducimus quaerat deserunt ut magnam nostrum quam, saepe repellat aspernatur praesentium labore ullam fuga illum.</span>
+            <div id="linha-horizontal"></div>
+        </div>
+        <!-- #################### -->
+        
     </div>
 
 </body>
