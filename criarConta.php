f3be9f098e6b9da2fac0431b4f5a99691b4d5ef8
Leandro Souza <leandrosouza6374@gmail.com>
2022-05-12 14:39:20 -0300

    0

--- criarConta.php
+++ criarConta.php
@@ -1,8 +1,29 @@
+<?php 
+ include("audioConexao.php");
+ $msg = false;
 
-<form action="audioCriarConta.php" method="POST">
-    <input type="file" name="arquivo">
+ if(isset($_FILES['arquivo'])){
+	 $extensao = strtolower(substr($_FILES['arquivo']['name'], -4));
+	 $novo_nome = md5(time()) . $extensao;
+	 $diretorio = "img/usuario/";
 
-        <div class="field">
+	 move_uploaded_file($_FILES['arquivo']['tmp_name'], $diretorio.$novo_nome);
+
+	 $usuario = $_POST["usuario"];
+	 $senha =  md5($_POST["senha"]);
+
+	 $sql_code = "INSERT INTO usuario (usuario, senha,arquivo, data) VALUES('$usuario', '$senha','$novo_nome', NOW())";
+	 if($conn->query($sql_code)){
+	    $msg = "Arquivo Enviado com Sucesso!";}
+	 else{
+	    $msg = "Falha ao enviar arquivo!";}
+ }
+?>
+
+<h1>Upload de Arquivos</h1>
+<?php if($msg != false) echo "<p> $msg </p>";?>
+<form action="criarConta.php" method="POST" enctype="multipart/form-data">
+<div class="field">
             <div class="control">
                     <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
             </div>
@@ -13,5 +34,8 @@
                      <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
             </div>
         </div>
-        <button type="submit" class="button is-block is-link is-large is-fullwidth">Criar Conta</button>
+	<input type="file" name="arquivo">
+	<input type="submit" value="Salvar">
 </form>
+
+
--- partes/painelAudio.php
+++ partes/painelAudio.php
@@ -1,7 +1,9 @@
 <div class="border " style="width:450px; height:300px; margin: 100px">
         <!-- BLOCO 1 : IMAGEM E NOME -->
         <div id="bloco1PainelAudio">
-        <img id="imagemPainelAudio" class="border rounded-circle border-dark" src="img/play.png" style="">
+            
+        <img id="imagemPainelAudio" class="border rounded-circle border-dark" src="img/usuario/402b016c739fe59f63fbfbe77cd4f449.jpg" style="width:600px; height: 120px; object-fit: cover; object-position: center;">
+
         <span id="nomePainelAudio"><?php echo $dado["usuario"] ?></span>
         <hr>
         </div>
