addaec8d61cc37b1ba955e01d32578c1cd6ee39d
Leandro Souza <leandrosouza6374@gmail.com>
2022-06-02 13:40:58 -0300

    .

--- partes/botaoEstrela.php
+++ partes/botaoEstrela.php
@@ -1,24 +1,22 @@
 <!-- IMAGEM BOTÃO ESTRELA -->
-<img onclick="Clicou<?php echo $dado['id'] ?>()" id="imgEstrela<?php echo $dado['id'] ?>" onmouseover="Estrela<?php echo $dado['id'] ?>()" src="img/star.png">
+<img onclick="Clicou<?php echo $dado['id'] ?>();" id="imgEstrela<?php echo $dado['id'] ?>" onmouseover="Estrela<?php echo $dado['id'] ?>()" src="img/star.png">
 <!-- FIM IMAGEM BOTÃO ESTRELA -->
 
 <!-- BOTÃO ESTRELA -->
 <input id="inputEstrela<?php echo $dado['id'] ?>"  type="submit" value="estrela" style="display: none"/>
 <!-- FIM BOTÃO ESTRELA -->
 
-<!-- SCRIPT QUE FAZ COM QUE SE APERTE O BOTÃO SUBMIT NO MOMENTO EM QUE EU APERTO NA IMAGEM DA ESTRELA -->
+
  <script>
         let img<?php echo $dado['id'] ?> = document.querySelector("#imgEstrela<?php echo $dado['id'] ?>")
         let btn<?php echo $dado['id'] ?> = document.querySelector("#inputEstrela<?php echo $dado['id'] ?>")
         console.log(btn<?php echo $dado['id'] ?>)
-
+//SCRIPT QUE FAZ COM QUE SE APERTE O BOTÃO SUBMIT NO MOMENTO EM QUE EU APERTO NA IMAGEM DA ESTRELA
         function Clicou<?php echo $dado['id'] ?>(){
         console.log("clicou na imagem")
         btn<?php echo $dado['id'] ?>.click()
-                }
-
-        function EstrelaHover<?php echo $dado['id'] ?>(){
-                img.src = "starHover.png"
         }
+//FIM SCRIPT
+
+
 </script>
-<!-- FIM SCRIPT -->
--- teste.php
+++ teste.php
@@ -1,10 +1,32 @@
-<img onmouseover="MudarEstrela()" id="imgEstrela" src="img/star.png">
+<img onmouseover="EstrelaHover()" onmouseout="EstrelaOut()" id="imgEstrela" src="img/star.png">
 
 <script>
- var imgEstrela = document.querySelector("#imgEstrela");
-//Função que faz com que a estrela mude
- function MudarEstrela(){
-	imgEstrela.src = "img/starHover.png";
+ var img = document.querySelector("#imgEstrela");
+//Função que faz com que a estrela mude quando passado o mouse sobre a imagem
+ function EstrelaHover(){
+	img.src = "img/starHover.png";
  }
- 
+ /////////////////////////////////////////////////////////////
+ //Função que faz com que a estrela mude quando o mouse sai da imagem
+ function EstrelaOut(){
+	img.src = "img/star.png";
+ }
+ /////////////////////////////////////////////////////////////
+
+
+
+
+ //Função que faz com que a estrela mude quando passado o mouse sobre a imagem
+function EstrelaHover<?php echo $dado['id'] ?>(){
+	img<?php echo $dado['id'] ?>.src = "img/starHover.png";
+ }
+ /////////////////////////////////////////////////////////////
+ //Função que faz com que a estrela mude quando o mouse sai da imagem
+ function EstrelaOut<?php echo $dado['id'] ?>(){
+	img<?php echo $dado['id'] ?>.src = "img/star.png";
+ }
+ /////////////////////////////////////////////////////////////
 </script>
+
+
+
