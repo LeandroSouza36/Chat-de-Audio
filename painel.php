52eeabe3dcd1b08bfebad1df2173c3878dc7b479
Leandro Souza <leandrosouza6374@gmail.com>
2022-06-14 01:26:52 -0300

    ..

--- painel.php
+++ painel.php
@@ -17,27 +17,24 @@ include('verifica_login.php');
 </head>
 
 <body style="padding: 20px; background-color: #fefbff">
-<?php
+    <?php
 
- include("audioConexao.php");
- $consultaAudio = "SELECT * FROM chataudio";
- $conAudio = $conn->query($consultaAudio) or die($conn->error);
+    include("audioConexao.php");
+    $consultaAudio = "SELECT * FROM chataudio";
+    $conAudio = $conn->query($consultaAudio) or die($conn->error);
 
- $consultaAudioStory = "SELECT * FROM chataudiostory";
- $conAudioStory = $conn->query($consultaAudioStory) or die($conn->error);
+    $consultaAudioStory = "SELECT * FROM chataudiostory";
+    $conAudioStory = $conn->query($consultaAudioStory) or die($conn->error);
 
-?>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
-    <script
-        src='https://code.jquery.com/jquery-3.2.1.slim.js'
-        integrity='sha256-tA8y0XqiwnpwmOIl3SGAcFl2RvxHjA8qp0+1uCGmRmg='
-        crossorigin='anonymous'
-    ></script>
+    ?>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+    <script src='https://code.jquery.com/jquery-3.2.1.slim.js' integrity='sha256-tA8y0XqiwnpwmOIl3SGAcFl2RvxHjA8qp0+1uCGmRmg=' crossorigin='anonymous'></script>
     <!-- STORYS DESATIVADO TEMPORARIAMENTE
     <div class="row" >
 
         
-        <?php // include('partes/botaoGravarStory.php'); ?>
+        <?php // include('partes/botaoGravarStory.php'); 
+        ?>
        
         <form name="form1" method="POST" action="audioEnvioStory.php">
             <input name="audio" type="text" id="testeStory" style-"display:none"></p>
@@ -45,120 +42,127 @@ include('verifica_login.php');
         </form>
 
         <div id="painelStory" class="border" style="width:900px;height:100px;margin: 30px; padding : 1px;background-color: #fcd6f9;">
-            <?php 
-               // while($dado = $conAudioStory->fetch_array() ){
+            <?php
+            // while($dado = $conAudioStory->fetch_array() ){
             ?>
             
-            <?php // include('partes/painelStory.php'); ?>
+            <?php // include('partes/painelStory.php'); 
+            ?>
              
-            <?php // }?>
+            <?php // }
+            ?>
         </div>
     </div>
     -->
     <div><img src="img/retroway.png"></div>
-    <hr/>
+    <hr />
     <div class="row">
-    <!-- MENU 2 (O DA ESQUERDA ) -->
-    <div class="row-md-2"></div>
-    <div class="col border">
-        <?php include('partes/menu.php'); ?>
-    </div> 
-    <!-- MENU 2 (O DA ESQUERDA ) FIM -->
+        <!-- MENU 2 (O DA ESQUERDA ) -->
+        <div class="row-md-2"></div>
+        <div class="col border">
+            <?php include('partes/menu.php'); ?>
+        </div>
+        <!-- MENU 2 (O DA ESQUERDA ) FIM -->
         <div class="col-md-8">
             <div class="row border">
                 <?php include('partes/botaoGravarAudio.php'); ?>
                 <?php include('partes/audioAmostra.php'); ?>
             </div>
 
-                <form name="form1" method="POST" action="audioEnvio.php">
-                    <input name="audio" type="text" id="testeGravar" style="display:none"></p>
+            <form name="form1" method="POST" action="audioEnvio.php">
+                <input name="audio" type="text" id="testeGravar" style="display:none"></p>
 
-                    <input id="inputConfirmarAudio" type="submit" value="confirmar"/>
-                </form>
+                <input id="inputConfirmarAudio" type="submit" value="confirmar" />
+            </form>
 
-                
-</script>
+
+            </script>
             <p>LISTA</p>
-               <!--  PAINEL DO AUDIO -->
-                <?php 
-                    while($dado = $conAudio->fetch_array() ){
-                ?>
-                   <?php include('partes/painelAudio.php'); ?>
-            <?php }?>
+            <!--  PAINEL DO AUDIO -->
+            <?php
+            while ($dado = $conAudio->fetch_array()) {
+            ?>
+                <?php include('partes/painelAudio.php'); ?>
+            <?php } ?>
             <!-- ################################# -->
         </div>
         <div class="row-md-2"></div>
         <div class="col border">
             <?php include('partes/menu.php'); ?>
-        </div>  
+        </div>
     </div>
-  
+
     <script src="assets/bootstrap/js/bootstrap.min.js"></script>
-<script>
-        $(function(){
+    <script>
+        $(function() {
             let mediaRecorder
 
-            navigator.mediaDevices.getUserMedia({ audio: true }).then(
-          (stream) => {
-            mediaRecorder = new MediaRecorder(stream)
-            let chunks = []
-            mediaRecorder.ondataavailable = data => {
-                chunks.push(data.data)
-            }
-            mediaRecorder.onstop = () => {
-                const blob = new Blob(chunks, { type: 'audio/ogg; code=opus' })
-  const reader = new window.FileReader()
-  reader.readAsDataURL(blob)
-  reader.onloadend = () => {
-    const audio = document.createElement('audio')
-    audio.src = reader.result
-    audio.controls = true
-    $('body').append(audio)
-    $testeGravar = reader.result;
-            function open_popup(date_today) {
-            document.querySelector("#testeGravar").value = date_today;
-            //Nessa Função abaixo eu criei uma variavel chamada 'amostraAudio' que chama uma div sem conteudo, depois eu modifiquei o conteudo da div colocando o audio que foi gravado.
-            let amostraAudio = document.getElementById("amostraAudio")
-            amostraAudio.innerHTML ="<audio src='"+date_today+"' controls=''></audio>";
-            console.log(amostraAudio.innerText)
-            ////////////////////////
+            navigator.mediaDevices.getUserMedia({
+                audio: true
+            }).then(
+                (stream) => {
+                    mediaRecorder = new MediaRecorder(stream)
+                    let chunks = []
+                    mediaRecorder.ondataavailable = data => {
+                        chunks.push(data.data)
+                    }
+                    mediaRecorder.onstop = () => {
+                        const blob = new Blob(chunks, {
+                            type: 'audio/ogg; code=opus'
+                        })
+                        const reader = new window.FileReader()
+                        reader.readAsDataURL(blob)
+                        reader.onloadend = () => {
+                            const audio = document.createElement('audio')
+                            audio.src = reader.result
+                            audio.controls = true
+                            $('body').append(audio)
+                            $testeGravar = reader.result;
 
-            }
+                            function open_popup(date_today) {
+                                document.querySelector("#testeGravar").value = date_today;
+                                //Nessa Função abaixo eu criei uma variavel chamada 'amostraAudio' que chama uma div sem conteudo, depois eu modifiquei o conteudo da div colocando o audio que foi gravado.
+                                let amostraAudio = document.getElementById("amostraAudio")
+                                amostraAudio.innerHTML = "<audio src='" + date_today + "' controls=''></audio>";
+                                console.log(amostraAudio.innerText)
+                                ////////////////////////
 
-            open_popup($testeGravar);
+                            }
+
+                            open_popup($testeGravar);
 
 
-  }
+                        }
 
-            }
-            
-            $('#btnStart').click(function(){
-         
-         if($(this).text() === 'Gravar'){       
-             mediaRecorder.start()
-             $(this).text('Gravando')
-         }else{
-             mediaRecorder.stop() 
-             $(this).text('Gravar')
-         }
-     })
-     $('#btnStory').click(function(){
-         
-         if($(this).text() === 'Gravar Story'){       
-             mediaRecorder.start()
-             $(this).text('Gravando')
-         }else{
-             mediaRecorder.stop() 
-             $(this).text('Gravar Story') 
-         }
-     })
-          },
-          (err) => {
-            $('body').append('<p>Você deve permitir o áudio</p>')
-          })   
-              
+                    }
+
+                    $('#btnStart').click(function() {
+
+                        if ($(this).text() === 'Gravar') {
+                            mediaRecorder.start()
+                            $(this).text('Gravando')
+                        } else {
+                            mediaRecorder.stop()
+                            $(this).text('Gravar')
+                        }
+                    })
+                    $('#btnStory').click(function() {
+
+                        if ($(this).text() === 'Gravar Story') {
+                            mediaRecorder.start()
+                            $(this).text('Gravando')
+                        } else {
+                            mediaRecorder.stop()
+                            $(this).text('Gravar Story')
+                        }
+                    })
+                },
+                (err) => {
+                    $('body').append('<p>Você deve permitir o áudio</p>')
+                })
+
         })
-      </script>
+    </script>
 
 
 </body>
