08d91ba7290b2a3b16b0101fd574f0b0f88951f8
Leandro Souza <leandrosouza6374@gmail.com>
2022-06-26 00:07:27 -0300

    som abrir painel

--- partes/funcaoAbrirPainelAmostraAudio.php
+++ partes/funcaoAbrirPainelAmostraAudio.php
@@ -4,5 +4,14 @@
 	function AbrirPainelAmostraAudio(){
 		let painelSuperAmostraAudio = document.getElementById("painelSuperAmostraAudio");
         painelSuperAmostraAudio.style.display = "inline";
-    }
+
+		let context = new AudioContext(),
+		oscillator = context.createOscillator(),
+		contextGain = context.createGain();
+		oscillator.type = 'sine';
+		oscillator.connect(contextGain);
+		contextGain.connect(context.destination);
+		oscillator.start(0);
+		contextGain.gain.exponentialRampToValueAtTime(0.00001, context.currentTime + 0.4)
+	}
     
--- teste.php
+++ teste.php
@@ -1,12 +1,15 @@
 <script>
 function  Psaa(){
-    let context = new AudioContext(),
-    oscillator = context.createOscillator();
-
-    oscillator.type = 'sine';
-    oscillator.connect(context.destination);
-    oscillator.start();
-    setTimeout(function() { oscillator.stop() }, 500);
+let context = new AudioContext(),
+oscillator = context.createOscillator(),
+contextGain = context.createGain();
+oscillator.type = 'sine';
+oscillator.connect(contextGain);
+contextGain.connect(context.destination);
+oscillator.start(0);
+contextGain.gain.exponentialRampToValueAtTime(
+  	0.00001, context.currentTime + 0.4
+	)
 
 }
     
